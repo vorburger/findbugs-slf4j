ef09fd098603f667e5929b93162e80501343af7b
Michael Vorburger <mike@vorburger.ch>
2018-03-14 16:05:18 +0100

    {WIP} fail even if only getMessage() and no cause (fixes #69)
    Signed-off-by: Michael Vorburger <mike@vorburger.ch>

--- test-case/src/main/java/pkg/UsingGetMessage.java
+++ test-case/src/main/java/pkg/UsingGetMessage.java
@@ -7,6 +7,7 @@ public class UsingGetMessage {
     private final Logger logger = LoggerFactory.getLogger(getClass());
 
     void method(Throwable t) {
+        logger.info("My message is {}", t.getMessage());
         logger.info("My message is {}", t.getMessage(), t);
         logger.info("My {} is {}", new Object[]{ "message", t.getMessage(), t });
         logger.info("My {} {} {}", new Object[]{ "message", "is", t.getMessage(), t });
